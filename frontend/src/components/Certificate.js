4413bd330a3d80f46d86744dd2dd84283720a22f
ZunairaAnsari <zunairaansari413@gmail.com>
2024-07-21 14:37:34 +0500

    completed the certificate

--- frontend/src/components/Certificate.js
+++ frontend/src/components/Certificate.js
@@ -1,21 +1,33 @@
 import React from "react";
 import "./Certificate.css"; // Import CSS for styling
+import backgroundImage from "../assets/abstract-geometric-background-in-flat-design-free-vector.jpg"; // Adjust the path as needed
+import logo from "../assets/smit.png";
 
 const Certificate = ({ recipientName, courseName, completionDate }) => {
+  const containerStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    width: "100%",
+  };
+
   return (
-    <div className="certificate-container">
+    <div className="certificate-container" style={containerStyle}>
       <div className="certificate-header">
-        <h2 className="certificate-title">Certificate of Completion</h2>
+        <img src={logo} />
+        <h1 className="certificate-title">Certificate of Completion</h1>
       </div>
       <div className="certificate-body">
-        <div className="certificate-text">
-          <p>This certifies that</p>
-          <h1 className="recipient-name">{recipientName}</h1>
-          <p>has successfully completed the course</p>
-          <h3 className="course-name">{courseName}</h3>
-          <p>awarded this {completionDate}</p>
-        </div>
+        <p>This certifies that</p>
+        <h1 className="recipient-name">Jhon Doe{recipientName}</h1>
+        <p>has successfully completed the course</p>
+        <h3 className="course-name">Web Development{courseName}</h3>
+        <p>awarded this on {completionDate}</p>
+        <br />
+        <p>12-July-2024</p>
+
         <div className="certificate-signature">
+          <hr className="hr" />
           <p>Authorized Signature</p>
         </div>
       </div>
