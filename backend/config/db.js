e463a8ad1eaa5ec0c63adf2281a75c03570c4815
ZunairaAnsari <zunairaansari413@gmail.com>
2024-07-31 19:59:08 +0500

    made some modifications in db connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,37 @@
+// const mongoose = require("mongoose");
+// const dotenv = require("dotenv");
+
+// dotenv.config();
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log("Your database is connected");
+//   } catch (err) {
+//     console.error(err.message);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
+
+require("dotenv").config(); 
+
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
 
-dotenv.config();
+const mongoURI = process.env.MONGO_URI;
 
-const connectDB = async () => {
+const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Your database is connected");
+    await mongoose.connect(mongoURI);
+    console.log("Successfully connected to MongoDB");
   } catch (err) {
-    console.error(err.message);
-    process.exit(1);
+    console.error("Failed to connect to MongoDB:", err);
   }
 };
 
-module.exports = connectDB;
+module.exports = connectDb;
