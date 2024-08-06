bccd8587084a9edda36b7f3740573541667df610
AtroobaIqbal <atroobaiqbal28@gmail.com>
2024-08-07 01:54:53 +0500

    add uuid to controller

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,12 @@
+const { v4: uuidv4 } = require('uuid');
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    default: uuidv4,
+  },
   name: {
     type: String,
     required: true,
