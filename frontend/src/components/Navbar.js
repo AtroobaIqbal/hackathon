586a132456a627c79725474b2ff77194babfac60
Tia-sheikh <142103997+Tia-sheikh@users.noreply.github.com>
2024-08-09 21:32:21 +0500

    changes

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,29 +2,32 @@ import React from 'react';
 import './Navbar.css'; // Import the CSS file for styling
 import logo2 from '../assets/smit.png'; // Import your logo image
 import { Link } from 'react-router-dom';
-import { FaBars, FaSignOutAlt } from 'react-icons/fa'; // Import React Icons
+import {  FaSignOutAlt } from 'react-icons/fa'; // Import React Icons
 
 const Navbar = ({ onToggleSidebar }) => {
     const handleLogout = () => {
         // Implement logout logic here, e.g., clearing auth tokens
         window.location.href = './signup'; // Redirect to login page
     };
-    return (
+
+    return ( 
+        <div>
             <nav className="navbar">
-                <button className="navbar-toggle" onClick={onToggleSidebar}>
-                    <FaBars /> {/* Font Awesome hamburger icon */}
-                </button>
-                <div className="navbar-logo">
+                <button onClick={onToggleSidebar}>
+                <div className="navbar-logo" >
                     <img src={logo2} alt="Logo" className="navbar-logo-img" />
-                    <ul className='navbar-link'>
-                      <Link to="/" className="navbar-link">Home</Link>
-                      </ul>  
-                      <button className="navbar-logout" onClick={handleLogout} >
-                    <FaSignOutAlt /> {/* Logout icon */ }
+                </div></button>
+                <ul className='navbar-links'> {/* Ensure CSS class matches */}
+                    <li>
+                        <Link to="/" className="navbar-link">Home</Link>
+                    </li>
+                    {/* Add more links here as needed */}
+                </ul>
+                <button className="navbar-logout" onClick={handleLogout}>
+                    <FaSignOutAlt /> {/* Logout icon */}
                 </button>
-                </div>
-            
             </nav>
+        </div>
     );
 };
 
