268f104285583311ab8fc36f79f5b770573aea8e
Tia-sheikh <142103997+Tia-sheikh@users.noreply.github.com>
2024-08-10 20:25:33 +0500

    g

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import NavbarWrapper from '../components/NavbarWrapper'; // Adjust path if needed
 import Sidebar from '../components/Sidebar'; // Adjust path if needed
 import image from '../assets/smit.png'; 
+import image2 from '../assets/image.png'
 import './HomePage.css'; // Import specific CSS for HomePage
 
 const HomePage = () => {
@@ -21,9 +22,13 @@ const HomePage = () => {
             <Sidebar isOpen={isSidebarOpen} onClose={closeSidebar} />
             <main className={`home-main-content ${isSidebarOpen ? 'shifted' : ''}`}>
                 <div className="content">
-                    <img className='image' src={image} alt="Background" />
-                    <h1>GENERATE YOUR CERTIFICATE HERE !!</h1>
-                    <a href="/courses" className="link">View Courses</a>
+                     <div className='image-container'>  
+                      <img className='image' src={image} alt="Background" />
+                      <img className='image2' src={image2} alt="Background" />
+                     <div className='heading'> <h1>GENERATE YOUR CERTIFICATE HERE !!</h1>
+                      <a href="/courses" className="link">View Courses</a></div>
+                     </div>
+                   
                 </div>
             </main>
         </div>
